33a8b772b376d0d9420bc1e29fa07c98301bfac7
LUZROLON-17 <rolonluz26@gmail.com>
2024-06-24 13:44:28 -0400

    actualizacion

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+
+        <h1>Hola Dojo!</h1>
+      <h2>
+        Things I need to do:
+        </h2>
+      <ul class="dojo">
+        <li>Lear React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>   
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
